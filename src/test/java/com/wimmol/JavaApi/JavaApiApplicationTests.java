6fabde79726ee67e36b27c777a929c5db824ea2b
Pavel Zaitsev <103185102+pavel-zaitsev-uptech@users.noreply.github.com>
2023-02-11 00:41:15 +0200

    Init commit spring.start.io

---
+++ src/test/java/com/wimmol/JavaApi/JavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimmol.JavaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
